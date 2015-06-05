2016ab8c5af207b07c12876b2fb86abfb45fa0b7
Rich Coleman <Richard.Coleman@activision.com>
2015-06-05 11:44:07 -0700

    added model outline

--- challenge.R
+++ challenge.R
@@ -4,7 +4,7 @@ library(sqldf)
 # NOTE: excel truncates profile_id values. we need to load data in directly from raw files!
 dfListens <- read.table("C:/Users/rcoleman/Documents/GitHub/iHeartRadio-Challenge/iHeartRadio datasets/listens.tsv",sep="",header=TRUE)
 
-dfArtists <- read.csv("C:/Users/rcoleman/Documents/GitHub/iHeartRadio-Challenge/iHeartRadio datasets/artist.csv")
+dfArtists <- read.delim("C:/Users/rcoleman/Documents/GitHub/iHeartRadio-Challenge/iHeartRadio datasets/artists.tsv")
 
 dfUsers <- read.table("C:/Users/rcoleman/Documents/GitHub/iHeartRadio-Challenge/iHeartRadio datasets/users.tsv",sep="",header=TRUE)
 
@@ -37,3 +37,87 @@ sqldf("select count(*) from dfActiveListeners")
 
 summary(dfInactiveListeners$age)
 sqldf("select count(*) from dfInactiveListeners")
+
+########################################Question 3#############################################
+
+colnames(dfListens)[3] <-  "artist_id"
+
+dfTemp <- merge(dfArtists,dfListens,by="artist_id")
+
+dfFinal <- merge(dfTemp,dfUsers,by="profile_id")
+
+top10Both <- sqldf("
+select genre 
+  ,sum(tracks_listened_to) as total_listens 
+from dfFinal 
+where genre is not null
+group by 1 
+order by 2 desc limit 10")
+
+top10Female <- sqldf("
+select genre 
+  ,sum(tracks_listened_to) as total_listens 
+from dfFinal 
+where genre is not null and gender='FEMALE'
+group by 1 
+order by 2 desc limit 10")
+
+top10Male <- sqldf("
+select genre 
+  ,sum(tracks_listened_to) as total_listens 
+from dfFinal 
+where genre is not null and gender='MALE'
+group by 1 
+order by 2 desc limit 10")
+
+dfFinalTop10Both <- merge(dfFinal,top10Both,by="genre")
+
+dfFinalTop10Female <- merge(dfFinal,top10Female,by="genre")
+dfFinalTop10Male <- merge(dfFinal,top10Male,by="genre")
+
+genreByGenderBoth <- sqldf("
+SELECT genre
+	,gender
+	,avg(age) AS avg_age
+	,sum(tracks_listened_to) AS total_listens
+FROM dfFinalTop10Both
+GROUP BY 1
+	,2
+ORDER BY 4 DESC
+")
+
+genreByGenderFemale <- sqldf("
+SELECT genre
+	,avg(age) AS avg_age
+	,sum(tracks_listened_to) AS total_listens
+FROM dfFinalTop10Female
+where gender='FEMALE'
+GROUP BY 1
+ORDER BY 3 DESC
+")
+
+genreByGenderMale <- sqldf("
+SELECT genre
+	,avg(age) AS avg_age
+	,sum(tracks_listened_to) AS total_listens
+FROM dfFinalTop10Male
+where gender='MALE'
+GROUP BY 1
+ORDER BY 3 DESC
+")
+
+#both gender
+ggplot(data=genreByGender,aes(x=genre)) +
+  geom_bar(data=subset(genreByGender,gender=="FEMALE"),aes(y=avg_age,fill='FEMALE'),alpha=.5,stat="identity") +
+  geom_bar(data=subset(genreByGender,gender=="MALE"),aes(y=avg_age,fill='MALE'),alpha=.5,stat="identity") +
+  labs(title="Average Age by Genre (Top 10 Genres Overall)",y="Average Age",x="Top 10 Genres",fill='Gender')
+
+#female
+ggplot(data=genreByGenderFemale,aes(x=genre,y=avg_age)) +
+  geom_bar(fill="pink",stat="identity") +
+  labs(title="Average Age by Genre (Top 10 Genres for Females)",y="Average Age",x="Top 10 Genres")
+
+#male
+ggplot(data=genreByGenderMale,aes(x=genre,y=avg_age)) +
+  geom_bar(fill="blue",stat="identity") +
+  labs(title="Average Age by Genre for Males (Top 10 Genres for Males)",y="Average Age",x="Top 10 Genres")
